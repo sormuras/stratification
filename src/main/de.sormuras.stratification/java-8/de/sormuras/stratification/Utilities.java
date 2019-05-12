51127704dfbabb4159029611eb8e189ed536f87f
Christian Stein <sormuras@gmail.com>
2019-05-12 15:16:35 +0200

    Add missing javadoc comments

--- src/main/de.sormuras.stratification/java-8/de/sormuras/stratification/Utilities.java
+++ src/main/de.sormuras.stratification/java-8/de/sormuras/stratification/Utilities.java
@@ -7,9 +7,18 @@ import java.net.URLClassLoader;
 public interface Utilities {
   /**
    * Get the {@link ClassLoader} providing access to just the classes of the runtime Java platform.
+   *
+   * @return the platform class loader
    */
   ClassLoader getPlatformClassLoader();
 
-  /** Create new {@link URLClassLoader} instance. */
+  /**
+   * Create new {@link URLClassLoader} instance.
+   *
+   * @param name class loader name; or {@code null} if not named
+   * @param parent the parent class loader for delegation
+   * @param urls the URLs from which to load classes and resources
+   * @return new instance of {@link URLClassLoader}
+   */
   URLClassLoader newUrlClassLoader(String name, ClassLoader parent, URL... urls);
 }
